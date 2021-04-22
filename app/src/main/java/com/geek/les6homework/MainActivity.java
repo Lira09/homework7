1b838896e0993108a310e19628a4788fab3ffad2
Lira09 <nazirbekova.lira@gmail.com>
2021-04-22 18:19:23 +0600

    calculator

--- app/src/main/java/com/geek/les6homework/MainActivity.java
+++ app/src/main/java/com/geek/les6homework/MainActivity.java
@@ -3,6 +3,8 @@ package com.geek.les6homework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,9 +14,9 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView textView;
-    private EditText editText;
-    private EditText editText1;
-    String myString = null;
+    private EditText editEmail;
+    private EditText editPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,17 +24,60 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.bn_sn);
         textView = findViewById(R.id.text);
-        editText = findViewById(R.id.email);
-        editText1= findViewById(R.id.password);
+        editEmail = findViewById(R.id.email);
+        editPassword= findViewById(R.id.password);
+
+        editPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!editEmail.getText().toString().equals("") && !editPassword.getText().toString().equals("")){
+                    button.setEnabled(true);
+                    button.setBackground(getResources().getDrawable(R.drawable.btn_white));
+                } else {
+                    button.setBackground(getDrawable(R.drawable.button));
+                }
+            }
+        });
+
+        editPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (!editEmail.getText().toString().equals("") && !editPassword.getText().toString().equals("")){
+                    button.setEnabled(true);
+                    button.setBackground(getResources().getDrawable(R.drawable.btn_white));
+                } else {
+                    button.setBackground(getDrawable(R.drawable.button));
+                }
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (editText.getText().toString().isEmpty() && editText1.getText().toString().isEmpty()){
-                    Toast.makeText(MainActivity.this,"Enter data!" , Toast.LENGTH_LONG).show();
-                }else {
-                    Toast.makeText(MainActivity.this, "Welcome!" ,Toast.LENGTH_LONG).show();
-                    button.setBackgroundColor(getResources().getColor(R.color.white));
+                if (editEmail.getText().toString().equals("") || editPassword.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "Enter data!", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Welcome!", Toast.LENGTH_LONG).show();
                 }
             }
         });
